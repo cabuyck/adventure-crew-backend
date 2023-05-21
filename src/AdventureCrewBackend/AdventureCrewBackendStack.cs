05ef08355b7a0b6f07ee661229b5f2b9de011c3c
Chris Buyck <cabuyck@gmail.com>
2023-05-20 20:05:49 -0500

    Initial commit

---
+++ src/AdventureCrewBackend/AdventureCrewBackendStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace AdventureCrewBackend
+{
+    public class AdventureCrewBackendStack : Stack
+    {
+        internal AdventureCrewBackendStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
